443a38e93bc75544e193392e4869f337af68f26d
yyyyccs7 <531036908@qq.com>
2020-05-09 11:46:50 -0500

    test Zhuping

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,7 +30,7 @@ export default function HomeScreen() {
           </View>
 
           <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
+            Change any of the text, save the file, and your app will automatically reload!!!!!
           </Text>
         </View>
 
