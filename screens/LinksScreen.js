2a7bc489dabffc76fc3be3ff3faeaa441d875df9
brians123 <brianssui@yahoo.com>
2020-05-14 10:17:00 -0400

    can drag and drop question pin; resized photo the fill up the screen while maintaining aspect ratio

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,7 +1,15 @@
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Animated,
+  PanResponder,
+} from "react-native";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -9,31 +17,57 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function LinksScreen() {
   const [annCoords, setAnnCoords] = useState([]);
 
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    })
+  ).current;
+
   const handleImageClick = (e) => {
     setAnnCoords([e.nativeEvent.locationX, e.nativeEvent.locationY]);
+    console.log(e);
   };
+  const deviceWidth = Dimensions.get("window").width;
+  const deviceHeight = Dimensions.get("window").height * 0.809;
 
   return (
-    <View>
-      <View style={styles.photoContainer}>
-        <Image
-          onTouchStart={(e) => handleImageClick(e)}
-          style={styles.photo}
-          resizeMode={"contain"}
-          source={{
-            uri:
-              "https://lh3.googleusercontent.com/proxy/gMrsrYWI3BmRkcFfQuRBAJCCr3LkfUaC0lnoxAsXqHQfNykkFKsBx1oH1YHeh9ENU7xKkvRfgXIRJasxt7J603Fx5JboWgIZyQ8j-Hju8xRTr5FVPrdCRH2oF7vsRD_b7gLhtbVeug",
-          }}
-        />
-      </View>
-      <View style={styles.pin}>
+    <View
+      style={{
+        borderColor: "red",
+        borderWidth: 2,
+        width: deviceWidth,
+        height: deviceHeight,
+      }}
+    >
+      <Image
+        onTouchStart={(e) => handleImageClick(e)}
+        style={styles.photo}
+        // resizeMode={"contain"}
+        source={{
+          uri:
+            "https://sputniktextbook.org/Pictures/About/TextBook/Page_118sm.jpg",
+        }}
+      />
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}
+      >
         <Pin coords={annCoords}></Pin>
-      </View>
-      {/* <MaterialCommunityIcons
-        name="map-marker-question"
-        size={24}
-        color="black"
-      /> */}
+      </Animated.View>
     </View>
   );
 }
@@ -54,23 +88,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#fafafa",
   },
   photo: {
-    width: 400,
-    height: 500,
+    width: undefined,
+    height: undefined,
     alignSelf: "center",
-    marginTop: 10,
-    position: "absolute",
+    // marginTop: 10,
+    // position: "absolute",
+    borderColor: "red",
+    borderWidth: 2,
+    resizeMode: "contain",
+    ...StyleSheet.absoluteFillObject,
   },
-  photoContainer: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
+  // photoContainer: {
+  //   position: "absolute",
+  //   top: 0,
+  //   bottom: 0,
+  //   left: 0,
+  //   right: 0,
+  //   borderColor: "red",
+  //   borderWidth: 2,
+  //   // borderTopWidth: 5,
+  // },
   pin: {
-    top: 0,
+    top: 10,
     bottom: 0,
-    left: 0,
+    left: 70,
     right: 0,
   },
 });
