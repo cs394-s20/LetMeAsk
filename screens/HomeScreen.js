39fc3fc06562038b8db992c84da218eb8d876cb8
Patrice Power <patricepower2021@u.northwestern.edu>
2020-05-19 20:18:43 -0400

    Styled cards and cleaned up homescreen.js code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,7 @@ import * as WebBrowser from 'expo-web-browser';
 import React, {useState} from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button, TextInput } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Avatar, Card, Title, Paragraph } from 'react-native-paper';
-
-import { MonoText } from '../components/StyledText';
-import { Camera } from 'expo-camera';
-
+import { Card, Title, Paragraph } from 'react-native-paper';
 
 
 export default function HomeScreen() {
@@ -28,13 +24,13 @@ export default function HomeScreen() {
       </View>
       
       <ScrollView horizontal={true} decelerationRate={0} snapToInterval={200} snapToAlignment={"center"}>
-        <Card style={{ height: '50%', backgroundColor: '#3686E4', margin:10 }}>
+        <Card style={styles.answeredQ}>
           <Card.Content>
             <Title>Do jellyfish sleep?</Title>
             <Paragraph>If so, how do they sleep?</Paragraph>
           </Card.Content>
         </Card>
-        <Card style={{ height: '50%', backgroundColor: '#3686E4', margin: 10 }}>
+        <Card style={styles.answeredQ}>
           <Card.Content>
             <Title>Do jellyfish sleep?</Title>
             <Paragraph>If so, how do they sleep?</Paragraph>
@@ -50,13 +46,13 @@ export default function HomeScreen() {
         snapToInterval={200}
         snapToAlignment={"center"}
         >
-        <Card style={{ height: '50%', backgroundColor: '#E14321', margin: 10 }}>
+        <Card style={styles.unansweredQ}>
           <Card.Content>
             <Title>Do jellyfish sleep?</Title>
             <Paragraph>If so, how do they sleep?</Paragraph>
           </Card.Content>
         </Card>
-        <Card style={{ height: '50%', backgroundColor: '#E14321', margin: 10 }}>
+        <Card style={styles.unansweredQ}>
           <Card.Content>
             <Title>Do jellyfish sleep?</Title>
             <Paragraph>If so, how do they sleep?</Paragraph>
@@ -71,40 +67,18 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
 
 const styles = StyleSheet.create({
+  answeredQ: {
+    height: '50%',
+    backgroundColor: '#3686E4',
+    margin: 10
+  },
+  unansweredQ: {
+    height: '50%',
+    backgroundColor: '#E14321',
+    margin: 10
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -118,77 +92,5 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
